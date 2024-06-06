3ea3c1ab6e3de8b51b6bdf6e82ed583a70adf5e2
Ekaterina Bondareva <1978231@bk.ru>
2024-06-06 15:31:31 +0500

    created side bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,39 @@
 import "../styles/SideBar.css";
+import first from "../images/first.svg";
+import second from "../images/second.svg";
+import third from "../images/third.svg";
+import fourth from "../images/fourth.svg";
+import fifth from "../images/fifth.svg";
+import sixth from "../images/sixth.svg";
+import seventh from "../images/seventh.svg";
+import eighth from "../images/eighth.svg";
+import ninth from "../images/ninth.svg";
+import tenth from "../images/tenth.svg";
 
 export function SideBar() {
+  const icons = [
+    first,
+    second,
+    third,
+    fourth,
+    fifth,
+    sixth,
+    seventh,
+    eighth,
+    ninth,
+    tenth,
+  ];
+  const icon = first;
   return (
     <div className="side-bar">
       <div className="side-bar__emblem"></div>
+      <div className="side-bar__admin-icon"></div>
+      {/* <div className="side-bar__icon"> */}
+      {icons.map((icon) => {
+        return <img className="side-bar__icon" src={icon} />;
+      })}
+      {/* <img src={icon} /> */}
+      {/* </div> */}
     </div>
   );
 }
