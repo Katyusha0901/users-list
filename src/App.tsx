8198854bf53563e04e05833931bc551ebee2b8b9
Ekaterina Bondareva <1978231@bk.ru>
2024-06-06 13:04:33 +0500

    added type interface of users and import users in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import users from "../public/users.json";
 
 export function App() {
   return <div className="App"></div>;
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface User {
+  name: string;
+  email: string;
+  permission: string[];
+  image: string;
+}
