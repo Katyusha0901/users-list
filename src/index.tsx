3b29151d07e35255f64174449d28612348b4a02f
Ekaterina Bondareva <1978231@bk.ru>
2024-06-07 09:33:37 +0500

    wrapped app by context

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
+import { PermissionsContextProvider } from "./PermissionsContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <PermissionsContextProvider>
+      <App />
+    </PermissionsContextProvider>
   </React.StrictMode>
 );
